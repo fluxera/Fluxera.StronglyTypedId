71fbcbdd4ca3d4cda29e5d9c572556b1bf4acde0
Matthias Gernand <mgernand@users.noreply.github.com>
2022-06-01 02:09:37 +0200

    Added a type converter for the strongly-typed IDs. (#4)

---
+++ tests/Fluxera.StronglyTypedId.UnitTests/Model/GuidId.cs
@@ -0,0 +1,12 @@
+﻿namespace Fluxera.StronglyTypedId.UnitTests.Model
+{
+	using System;
+
+	public class GuidId : StronglyTypedId<GuidId, Guid>
+	{
+		/// <inheritdoc />
+		public GuidId(Guid value) : base(value)
+		{
+		}
+	}
+}

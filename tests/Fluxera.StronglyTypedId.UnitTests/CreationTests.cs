b443369622b182eac5824ba90e73d466b5d051c3
Matthias Gernand <matthias.gernand@fluxera.com>
2023-01-22 16:32:17 +0100

    Added value constrait checks for string, Guid and numeric types.

---
+++ tests/Fluxera.StronglyTypedId.UnitTests/CreationTests.cs
@@ -0,0 +1,66 @@
+﻿namespace Fluxera.StronglyTypedId.UnitTests
+{
+	using System;
+	using FluentAssertions;
+	using Fluxera.StronglyTypedId.UnitTests.Model;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class CreationTests
+	{
+		[Test]
+		public void ShouldNotAllowNullValue_String()
+		{
+			Action action = () =>
+			{
+				StringId id = new StringId(null);
+			};
+
+			action.Should().Throw<ArgumentNullException>();
+		}
+
+		[Test]
+		public void ShouldNotAllowEmptyValue_String()
+		{
+			Action action = () =>
+			{
+				StringId id = new StringId(string.Empty);
+			};
+
+			action.Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		public void ShouldNotAllowEmptyValue_Guid()
+		{
+			Action action = () =>
+			{
+				GuidId id = new GuidId(Guid.Empty);
+			};
+
+			action.Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		public void ShouldNotAllowZeroValue_Numeric()
+		{
+			Action action = () =>
+			{
+				IntegerId id = new IntegerId(0);
+			};
+
+			action.Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		public void ShouldNotAllowNegativeValue_Numeric()
+		{
+			Action action = () =>
+			{
+				IntegerId id = new IntegerId(-1);
+			};
+
+			action.Should().Throw<ArgumentException>();
+		}
+	}
+}
